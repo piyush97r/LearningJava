9791f3a275f47a9c1e3006d136c8108915dff750
piyush97r <piyush97ranjan@gmail.com>
2020-12-03 23:51:19 +0530

    Example : Allocating Work Days using Switch

---
+++ src/com/company/WorkDay.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.*;
+
+public class WorkDay {
+
+    public static void main(String args[]) {
+
+        int day = 5;
+
+        switch(day) {
+
+            case 1:
+                System.out.print("Work Day-Monday");
+                break;
+            case 2:
+                System.out.print("Work Day-Tuesday");
+                break;
+            case 3:
+                System.out.print("Work Day-Wednesday");
+                break;
+            case 4:
+                System.out.print("Work Day-Thursday");
+                break;
+            case 5:
+                System.out.print("Work Day-Friday");
+                break;
+            default:
+                System.out.print("Yayy-Weekend");
+                break;
+        }
+    }
+}
