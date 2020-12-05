13ffec465aaac5cf66562e0e67d42b3f7048f0e2
piyush97r <piyush97ranjan@gmail.com>
2020-12-05 23:57:10 +0530

    WhileLoop2 Example

---
+++ src/com/company/WhileLoop2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class WhileLoop2 {
+    public static void main(String[] args){
+        int num = 1;
+        while(num<=100){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
