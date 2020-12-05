13ee0d1e7328c33efd9dfffa95bff1c5b9ccde20
piyush97r <piyush97ranjan@gmail.com>
2020-12-06 00:10:35 +0530

    WhileLoop Example

---
+++ src/com/company/OddNumbers.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class OddNumbers {
+    public static void main(String[] args){
+        int num = 25;
+        while(num<=25 && num >0){
+            System.out.println(num+ " ");
+            num = num-2;
+        }
+    }
+}
