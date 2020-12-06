228dae12f6a93dcf404140549d8aeb863b839926
piyush97r <piyush97ranjan@gmail.com>
2020-12-07 04:01:43 +0530

    Using Functions

---
+++ src/com/company/FunctionsClass.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class FunctionsClass {
+
+        public static void main(String[] args) {
+
+            double var = test();
+
+            System.out.print(var);
+
+        }
+
+        public static int test() {
+
+            int variable = 4;
+
+            return variable;
+
+        }
+
+    }
+
