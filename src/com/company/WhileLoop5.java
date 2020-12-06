dd77974a6433411db6bb17e389a700742b52054c
piyush97r <piyush97ranjan@gmail.com>
2020-12-06 08:13:26 +0530

    WhileLoop Example

---
+++ src/com/company/WhileLoop5.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class WhileLoop5 {
+    public static void main(String[] args) {
+        int x, n;
+        Scanner input = new Scanner(System.in);
+        x = input.nextInt();
+        n = input.nextInt();
+        int a = 1;
+        while ((x*a)<=(x*n)){
+            System.out.println(x*a);
+            a++;
+
+        }
+}}
+
