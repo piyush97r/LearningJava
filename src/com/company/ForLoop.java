16d8cb2602ef2bc6ec2cca144101aee9d1171c98
piyush97r <piyush97ranjan@gmail.com>
2020-12-06 19:28:44 +0530

    WhileLoop Example

---
+++ src/com/company/ForLoop.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ForLoop {
+    public static void main(String[] args){
+
+        for (int number = 1; number <= 100; number++) {
+            System.out.print(number + " ");
+            number++;
+        }
+    }
+}
