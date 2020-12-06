b3d7d715ee2f81b1fb3b38b5c867a937bcee39fa
piyush97r <piyush97ranjan@gmail.com>
2020-12-06 20:41:01 +0530

    doWhile Example

---
+++ src/com/company/doWhile2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class doWhile2 {
+    public static void main(String[] args){
+        int counter=0;
+        do{
+            counter=counter+1;
+            System.out.println(counter);
+        }while (counter<12);
+    }
+}
