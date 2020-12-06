90af821f3bc2877ea8b2c71da1adbea3a8dd00c2
piyush97r <piyush97ranjan@gmail.com>
2020-12-06 20:43:24 +0530

    doWhile Example

---
+++ src/com/company/doWhile3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class doWhile3 {
+        public static void main(String args[]) {
+            boolean test = false;
+
+            do {
+                System.out.println("Hello world");
+            } while (test);
+        }
+    }
+
