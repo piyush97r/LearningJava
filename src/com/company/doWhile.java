99b91903503c780361588434750d509ceab0126f
piyush97r <piyush97ranjan@gmail.com>
2020-12-06 20:39:20 +0530

    ForLoop Example

---
+++ src/com/company/doWhile.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class doWhile {
+    public static void main(String[] args) {
+        int i = 5;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 10);
+    }
+}
