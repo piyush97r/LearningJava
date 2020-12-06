fc5ab5d3ea9dfb17e7c291dd13b8106d50120607
piyush97r <piyush97ranjan@gmail.com>
2020-12-06 20:51:25 +0530

    doWhile Example

--- src/com/company/doWhile5.java
+++ src/com/company/doWhile5.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class doWhile5 {
+    public static void main(String[] args) {
+        int n;
+        Scanner input= new Scanner(System.in);
+        n=input.nextInt();
+        int counter=n;
+        do{
+            System.out.println(counter);
+            counter--;
+        }while(counter>=0);
+
+
+    }
 }
