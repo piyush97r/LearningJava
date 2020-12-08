ea344f8fac6177f5cf61d8068163f2d4a2236fe5
piyush97r <piyush97ranjan@gmail.com>
2020-12-08 21:39:57 +0530

    Java Examples

---
+++ src/com/company/DecimalToInteger.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class DecimalToInteger {
+    public static void main(String[] args) {
+        double[] array = {10.32,20.36,30.50,40.51,50.49,60.43,71.71};
+
+        for(int i = 0; i <  array.length; i++) {
+            // Write the round function here.
+            array[i]=Math.round(array[i]);
+        }
+
+
+        for(int i = 0; i <  array.length; i++) {
+            System.out.print((array[i])+(i==array.length?"":" "));
+
+        }
+
+    }
+}
---
+++ src/com/company/MathLibrary.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class MathLibrary {
+
+    public static void main(String[] args) {
+        //System.out.print(Math.sqrt(98));
+
+	   double absolute = Math.abs(9.657);
+	  System.out.println((int)absolute);
+
+        //System.out.print( Math.max(89, 64));
+
+    }
+
+}
+
