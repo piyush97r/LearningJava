73e9df2b8e9f802ca7a3103d00eb37e075024d77
piyush97r <piyush97ranjan@gmail.com>
2020-12-09 16:19:57 +0530

    Java Examples

---
+++ src/com/company/Test2.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Test2 {
+    public static void main(String args[])throws ArrayIndexOutOfBoundsException {
+        int[] numbers = {1,2,34,5,6,7,8,9,10,11,12,13,14,15};
+        fizzBuzz(numbers);
+
+    }
+
+    public static void fizzBuzz(int[] numbers) {
+        //Write the function body here
+         for(int i = 0; i<numbers.length;i++){
+            if(numbers[i]%3==0&& numbers[i]%5==0){
+                System.out.print("FizzBuzz");
+            } else if (numbers[i]%5==0) {
+                System.out.print("Buzz ");
+            }
+            else if (numbers[i]%3==0){
+                System.out.print("Fizz ");
+            }
+            else{
+                System.out.print(numbers[i]+" ");
+            }
+
+        }
+    }
+}
