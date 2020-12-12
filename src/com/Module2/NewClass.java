5d5d8954d606df8a11ed7d342dd3c86ae33412fb
piyush97r <piyush97ranjan@gmail.com>
2020-12-12 21:59:16 +0530

    Area of Circle using Functions

---
+++ src/com/Module2/NewClass.java
@@ -0,0 +1,11 @@
+package com.Module2;
+
+public class NewClass {
+    public static void main(String[] args){
+        float radius = 10.0f;
+        System.out.println(area(radius));
+    }
+    public static float area(float radius){
+        return 3.14f*radius*radius;
+    }
+}
