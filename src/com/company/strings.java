64dd30905a19c3fb0d79b4e29e95a6b41f5621c1
piyush97r <piyush97ranjan@gmail.com>
2020-12-12 20:57:52 +0530

    Java Examples

--- src/com/company/strings.java
+++ src/com/company/strings.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class strings {
     public static void main(String[] args)
     {
         char x = 'a';
-
+        Scanner input=new Scanner(System.in);
         // using strings
 
         String firstName = "John";
@@ -12,9 +14,11 @@ public class strings {
 
         String fullName = firstName + lastName;
         System.out.println(fullName);
+        System.out.println("Enter New Name");
+        String newName = input.next();
 
         System.out.println(firstName + " " + lastName);
         System.out.println("My name is: " + firstName);
-
+        System.out.println(newName);
     }
 }
