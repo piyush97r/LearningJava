e110f591884cad5bf096334ccd4261c0f8eb4c2c
piyush97r <piyush97ranjan@gmail.com>
2020-12-13 10:04:12 +0530

    Using Constructors

--- src/com/Module2/Student.java
+++ src/com/Module2/Student.java
@@ -1,31 +1,39 @@
 package com.Module2;
 
+
 public class Student {
 
     public static void main(String[] args){
         Outputs s1 = new Outputs();
         Outputs s2 = new Outputs();
         Outputs s3 = new Outputs();
-        s1.name = "Ankit";
-        s2.rollNo = 14;
-        s3.cgpa = 6.44;
-
+        s1.name="Ankit";
+        s2.rollNo=14;
+        s3.cgpa=7;
 
         System.out.println("First Student :"+s1.Outputing() );
-        System.out.println("First Student :"+s2.Outputing() );
-        System.out.println("First Student :"+s3.Outputing() );
+        System.out.println("First Student :"+s2.Outputing2() );
+        System.out.println("First Student :"+s3.Outputing3() );
 
     }
 }
 class Outputs{
-    public int rollNo;
     public String name;
+    public int rollNo;
     public double cgpa;
-    public String Outputing(){
-        return String.valueOf(rollNo);
 
-
-
+   public String Outputing() {
+        return name;
 
     }
-}
+    public int Outputing2() {
+        return rollNo;
+
+    }
+    public double Outputing3() {
+        return cgpa;
+
+    }
+
+    }
+
