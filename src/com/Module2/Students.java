45d8214927449ba44b00bcbfcc332e1a8ae43430
piyush97r <piyush97ranjan@gmail.com>
2020-12-14 20:54:03 +0530

    Circumference and Area of a Circle

---
+++ src/com/Module2/Students.java
@@ -0,0 +1,34 @@
+package com.Module2;
+
+public class Students {
+    public static void main(String[] args) {
+
+        // Create an object named 's1' and complete the code to print the desired output
+        Student1 s1 = new Student1(1056 , "karan" , 8.8);
+        s1.displayProfile();
+        System.out.println("Percentage of s1 is " +s1.findPercentage());
+    }
+}
+class Student1{
+    public int rollno;
+    public String name;
+    public double cgpa;
+
+    // Declare constructor of 'Student' class here
+    public Student1(int rollno ,  String name , double cgpa){
+        this.rollno = rollno;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public void displayProfile() {
+        System.out.println(rollno + " " + name + " " + cgpa);
+
+
+
+    }
+    public double findPercentage() {
+        double percent = cgpa * 10;
+        return percent;
+    }
+}
