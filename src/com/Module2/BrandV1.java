b30967ace7b05c9ab83d418ea80ecf53a62a5e04
piyush97r <piyush97ranjan@gmail.com>
2020-12-19 20:47:32 +0530

    Catalog Management System -  Brand Class

---
+++ src/com/Module2/BrandV1.java
@@ -0,0 +1,58 @@
+package com.Module2;
+
+public class BrandV1 {
+    private String name;
+    private String description;
+    private Image[] images;
+
+    /* Constructor for the class*/
+    public void BrandV1(String name){
+        this.name = name;
+        /*Initializing the array to 3 */
+        this.images = new Image[3];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Image[] getImage() {
+        return images;
+    }
+
+    public void setImage(Image[] images) {
+        this.images = images;
+    }
+
+    /** Method addImage. This iterates over the images array, if the array index is null the value is set. **/
+    public void addImage(Image image) {
+        for (int i = 0; i < this.images.length; i++) {
+            if (null == this.images[i]) {
+                this.images[i] = image;
+                return;
+            }
+        }
+    }
+
+    /** Method getDefaultImage returns the default image of the Brand **/
+    public Image getDefaultImage() {
+        for (int i = 0; i < this.images.length; i++) {
+            if (this.images[i].isDefaultImg()) {
+                return this.images[i];
+            }
+        }
+        return null;
+    }
+}
