1c3419fc645ff9f60b2f021da719bdc563fbc477
piyush97r <piyush97ranjan@gmail.com>
2020-12-20 17:24:22 +0530

    Test for Robot Case

---
+++ src/com/company/B.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class B {
+    // Other code here
+
+    public B() {
+        // default constructor code here
+        B b = new B();
+        b.passLocation(5,5);
+    }
+
+    // draw a line from current location to the location specified
+    public void passLocation(int x, int y) {
+        // more code
+    }
+}
