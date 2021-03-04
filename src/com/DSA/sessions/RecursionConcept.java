89fb27dc856cc2fe70b5f9a732b84563f820cef4
piyush97r <piyush97ranjan@gmail.com>
2021-03-04 22:13:53 +0530

    Factorial

--- src/com/DSA/sessions/RecursionConcept.java
+++ src/com/DSA/sessions/RecursionConcept.java
@@ -8,7 +8,7 @@ public class RecursionConcept {
             return 1;
         else
             return(n * factorial(n-1));
-    }
+    }//5*4*3*2*1 = 120
     public static void main(String args[]){
         int i,fact=1;
         int number=4;//It is the number to calculate factorial
