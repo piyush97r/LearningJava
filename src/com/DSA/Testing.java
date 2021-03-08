9c8ae25cb0de4e5e73be74c288bbb76158172c61
piyush97r <piyush97ranjan@gmail.com>
2021-03-08 19:15:42 +0530

    Testing Codes

---
+++ src/com/DSA/Testing.java
@@ -0,0 +1,44 @@
+package com.DSA;
+
+import java.util.*;
+
+public class Testing
+{
+    static long gpUtil(long r, long n, long m)
+    {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return (1 + r) % m;
+
+        long res;
+
+        if (n % 2 == 1)
+            res = (1 + r) * gpUtil((r * r) % m, (n - 1) / 2, m);
+        else
+            res = 1 + (r * (1 + r) * gpUtil((r * r) % m, (n / 2) - 1, m));
+
+        return (res % m);
+    }
+
+    static long gp(long a, long r, long n, long m)
+    {
+        long res;
+        res = a * gpUtil(r, n, m);
+        res = res % m;
+
+        return res;
+    }
+
+    public static void main(String args[])
+    {
+        int a, r, n, m;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        r = s.nextInt();
+        n = s.nextInt();
+        m = s.nextInt();
+
+        System.out.println(gp(a, r, n, m) + "\n" );
+    }
+}
