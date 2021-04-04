f2f1a75e9ee0b29aff1703b9046da69b2a27f35e
piyush97r <piyush97ranjan@gmail.com>
2021-04-04 20:46:09 +0530

    Testing Codes

---
+++ src/com/DSA/sessions/EmailString.java
@@ -0,0 +1,29 @@
+package com.DSA.sessions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.String;
+
+
+public class EmailString {
+    public static void main(String[] args)
+    {
+        //String[] emails = {"chandler.bing@xyz.com","ross.geller@xyz.com","rachel.greene@xyz.com","joey.tribbiani@xyz.com","monica.geller@xyz.com","phoebe.buffay@xyz.com","sheldon.copper@xyz.com","marie.george@xyz.com"};
+
+        String NewEmail="monica.geller@xyz.com";
+
+        List<String> emails = new ArrayList<String>();
+        emails.add("chandler.bing@xyz.com");
+        emails.add("ross.geller@xyz.com");
+        emails.add("rachel.greene@xyz.com");
+        emails.add("joey.tribbiani@xyz.com");
+        emails.add("monica.geller@xyz.com");
+        emails.add("phoebe.buffay@xyz.com");
+        emails.add("sheldon.copper@xyz.com");
+        emails.add("marie.george@xyz.com");
+
+        System.out.println("Testing for the further code. ");
+        }
+
+    }
+
